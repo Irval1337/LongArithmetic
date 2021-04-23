99234079326f61114042be3c3b85bf86a1f4a19a
Irval <56792892+Irval1337@users.noreply.github.com>
2021-04-23 20:57:08 +0300

    Add files via upload

---
+++ Example.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <conio.h>
+#include "large.hpp"
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    string num1, num2;
+    cout << "Первое число: ";
+    getline(cin, num1);
+    cout << "Первое число: ";
+    getline(cin, num2);
+    cout << "\nСумма: " << (large(num1) + large(num2)).ToString() << endl;
+    cout << "Разность: " << (large(num1) - large(num2)).ToString() << endl;
+    cout << "Произведение: " << (large(num1) * large(num2)).ToString() << endl;
+    cout << "Частное: " << (large(num1) / large(num2)).ToString() << endl;
+    cout << "Остаток от деления: " << (large(num1) % large(num2)).ToString() << endl;
+    cout << "A в степени B: " << (large(num1).pow(large(num2))).ToString() << endl;
+    _getch();
+}
