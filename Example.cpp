2bb46462fd79927d06a4f744bd1e3ee991e986a6
Irval <56792892+Irval1337@users.noreply.github.com>
2022-11-14 22:50:25 +0300

    Update Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -6,10 +6,10 @@ int main() {
     setlocale(LC_ALL, "RU");
     large num1, num2;
     cout << "Первое число: ";
-    getline(cin, num1);
-    cout << "Первое число: ";
-    getline(cin, num2);
-    cout << "\nСумма: " << num1 + num2 << endl;
+    cin >> num1;
+    cout << "Второе число: ";
+    cin >> num2;
+    cout << endl << "Сумма: " << num1 + num2 << endl;
     cout << "Разность: " << num1 - num2 << endl;
     cout << "Произведение: " << num1 * num2 << endl;
     cout << "Частное: " << num1 / num2 << endl;
